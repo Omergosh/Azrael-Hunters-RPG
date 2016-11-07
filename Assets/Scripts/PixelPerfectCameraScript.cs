6e950dac2da3d1a6341173da967dd13757b3d6ea
Omer Ahmed <omer1@ualberta.ca>
2016-11-06 18:51:58 -0700

    Added Existing Unity Project
    We're ready, fellas.

---
+++ Assets/Scripts/PixelPerfectCameraScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Adjusts the Camera's Orthographic Size to create a Pixel Perfect Camera at any resolution.
+/// </summary>
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PixelPerfectCameraScript : MonoBehaviour
+{
+	public Camera Camera
+	{
+		get { return _camera ?? (_camera = GetComponent<Camera>()); }
+	}
+
+	private Camera _camera;
+
+	public int PixelsPerUnit = 16;
+
+	private int _lastHeight;
+	private int _lastPPU;
+
+	public void Update()
+	{
+		if (_lastHeight == Screen.height && _lastPPU == PixelsPerUnit) return;
+
+		_lastHeight = Screen.height;
+		_lastPPU = PixelsPerUnit;
+
+		Camera.orthographicSize = Screen.height / (PixelsPerUnit * 2f);
+	}
+}
---
+++ Assets/Scripts/PixelSnapScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Snaps the attached GameObject to a Pixel Perfect X and Y position.
+/// </summary>
+[ExecuteInEditMode]
+public class PixelSnapScript : MonoBehaviour
+{
+    public int PixelsPerUnit = 16;
+
+    private float _lastX;
+    private float _lastY;
+
+    // Snap the attached GameObject to it's Pixel Perfect position.
+    public void Update()
+    {
+        if (gameObject.transform.position.x == _lastX && gameObject.transform.position.y == _lastY) return;
+
+        gameObject.transform.position = new Vector3(((float)((int)(gameObject.transform.position.x * PixelsPerUnit)) / PixelsPerUnit),
+                                                    ((float)((int)(gameObject.transform.position.y * PixelsPerUnit)) / PixelsPerUnit),
+                                                    gameObject.transform.position.z);
+        _lastX = gameObject.transform.position.x;
+        _lastY = gameObject.transform.position.y;
+    }
+
+    // Move the attached GameObject by a certain number of Pixels.
+    public void Move(int pixelX, int pixelY)
+    {
+        gameObject.transform.position = new Vector3(((float)((int)(gameObject.transform.position.x * PixelsPerUnit) + pixelX) / PixelsPerUnit),
+                                                    ((float)((int)(gameObject.transform.position.y * PixelsPerUnit) + pixelY) / PixelsPerUnit),
+                                                    gameObject.transform.position.z);
+        _lastX = gameObject.transform.position.x;
+        _lastY = gameObject.transform.position.y;
+    }
+}

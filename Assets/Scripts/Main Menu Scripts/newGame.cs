1663e146090d0fa11f3eeaa92bd5e8488f4ae0ee
Struckdown <bfleysher2@gmail.com>
2016-11-08 20:21:25 -0700

    New Game Button working

--- Assets/Scripts/Main Menu Scripts/newGame.cs
+++ Assets/Scripts/Main Menu Scripts/newGame.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class newGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void onClick(string level)
+    {
+        SceneManager.LoadScene(level);  //simply loading the map for now
+    }
 }

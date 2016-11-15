5bc11b7919753814cea2851c42e8102d55398daf
Struckdown <bfleysher2@gmail.com>
2016-11-14 21:21:29 -0700

    Added Player Movement
    Character can now move on the grid. No collisions. Haven't touched the
    camera though.

---
+++ Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PlayerControlsOverworld : MonoBehaviour {
+
+    Vector3 pos;                    // For movement
+    public float speed = 4.0f;      // Speed of movement
+
+    void Start()
+    {
+        pos = transform.position;          // Take the initial position
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && transform.position == pos)   // Left
+        {        
+            pos += 2 * Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position == pos)   // Right
+        {        
+            pos += 2 * Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W) && transform.position == pos)   // Up
+        {       
+            pos += 2 * Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position == pos)   // Down
+        {        
+            pos += 2 * Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Moving to new pos
+
+        //if (Input.GetKey(KeyCode.Z) //user interaction
+    }
+}

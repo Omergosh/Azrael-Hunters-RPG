cf377ca64091fbbe0afa27d367f7cb309f4ee4ac
Omer Ahmed <omer1@ualberta.ca>
2016-11-20 19:25:11 -0700

    Collision Detection implemented on Map Exploration
    The walls have risen up around us, imprenetable and strong! Fear not,
    sheeple! The flock shall not escape!

--- Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
+++ Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
@@ -6,34 +6,85 @@ public class PlayerControlsOverworld : MonoBehaviour {
 
     Vector3 pos;                    // For movement
     public float speed = 4.0f;      // Speed of movement
+    public string direction = "right";
+    public Vector2 directionV = Vector2.right;
+
+    //Collision stuff
+    int colLayerMask;
 
     void Start()
     {
         pos = transform.position;          // Take the initial position
+        colLayerMask = 1 << 8;//LayerMask.NameToLayer("Collisions");
     }
 
     void Update()
     {
         if (Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos)   // Left
-        {        
-            pos += 2 * Vector3.left;
+        {
+            directionV = Vector2.left;
+            if (CheckCollisions(directionV))
+            {
+                //Try to walk into wall sound effect
+            }else
+            {
+                //Successful walk!
+                pos += 2 * Vector3.left;
+            }
         }
         if (Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos)   // Right
-        {        
-            pos += 2 * Vector3.right;
+        {
+            directionV = Vector2.right;
+            if (CheckCollisions(directionV))
+            {
+                //Try to walk into wall sound effect
+            }
+            else
+            {
+                //Successful walk!
+                pos += 2 * Vector3.right;
+            }
         }
         if (Input.GetAxisRaw("Vertical") > 0 && transform.position == pos)   // Up
-        {       
-            pos += 2 * Vector3.up;
+        {
+            directionV = Vector2.up;
+            if (CheckCollisions(directionV))
+            {
+                //Try to walk into wall sound effect
+            }
+            else
+            {
+                //Successful walk!
+                pos += 2 * Vector3.up;
+            }
         }
         if (Input.GetAxisRaw("Vertical") < 0 && transform.position == pos)   // Down
-        {        
-            pos += 2 * Vector3.down;
+        {
+            directionV = Vector2.down;
+            if (CheckCollisions(directionV))
+            {
+                //Try to walk into wall sound effect
+            }
+            else
+            {
+                //Successful walk!
+                pos += 2 * Vector3.down;
+            }
         }
 
         transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Moving to new pos
 
         //if (Input.GetAxisRaw("Interact") > 0) //user interaction
         //if (Input.GetAxisRaw("Back") > 0) //abort! abort! you know, dialogue
+    }
+
+    bool CheckCollisions(Vector2 direction)
+    {
+        if (Physics2D.Raycast(transform.position, direction, 2.0f, colLayerMask))
+        {
+            return true;
+        }
+
+        return false;
     }
 }

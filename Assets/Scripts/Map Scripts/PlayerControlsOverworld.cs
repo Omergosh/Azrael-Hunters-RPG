fe56a556ccfde2d0c87e0223e528225941bbbd0f
Omer Ahmed <omer1@ualberta.ca>
2016-11-22 22:40:23 -0700

    Collisions & Basic Dialogue Stuff
    -Collisions re-implemented
    -Basic (Debug.Log) Dialogue added
    -NPC's can now exist! Yay.

--- Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
+++ Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
@@ -6,34 +6,107 @@ public class PlayerControlsOverworld : MonoBehaviour {
 
     Vector3 pos;                    // For movement
     public float speed = 4.0f;      // Speed of movement
+    public bool canMove = true;
+    public Vector2 directionV;
+    public int layerMaskCollisions = 1 << 8;
+    public int layerMaskInteracts = 1 << 9;
 
     void Start()
     {
         pos = transform.position;          // Take the initial position
+        directionV = Vector2.down;
     }
 
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos)   // Left
-        {        
-            pos += 2 * Vector3.left;
-        }
-        if (Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos)   // Right
-        {        
-            pos += 2 * Vector3.right;
-        }
-        if (Input.GetAxisRaw("Vertical") > 0 && transform.position == pos)   // Up
-        {       
-            pos += 2 * Vector3.up;
-        }
-        if (Input.GetAxisRaw("Vertical") < 0 && transform.position == pos)   // Down
-        {        
-            pos += 2 * Vector3.down;
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos)   // Left
+            {
+                directionV = Vector2.left;
+                if (CollisionCheck(directionV))
+                {
+                    //Walk into wall sound effect
+                }
+                else
+                {
+                    pos += 2 * Vector3.left;
+                }
+            }
+            if (Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos)   // Right
+            {
+                directionV = Vector2.right;
+                if (CollisionCheck(directionV))
+                {
+
+                }
+                else
+                {
+                    pos += 2 * Vector3.right;
+                }
+            }
+            if (Input.GetAxisRaw("Vertical") > 0 && transform.position == pos)   // Up
+            {
+                directionV = Vector2.up;
+                if (CollisionCheck(directionV))
+                {
+
+                }
+                else
+                {
+                    pos += 2 * Vector3.up;
+                }
+            }
+            if (Input.GetAxisRaw("Vertical") < 0 && transform.position == pos)   // Down
+            {
+                directionV = Vector2.down;
+                if (CollisionCheck(directionV))
+                {
+
+                }
+                else
+                {
+                    pos += 2 * Vector3.down;
+                }
+            }
         }
 
         transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Moving to new pos
 
-        //if (Input.GetAxisRaw("Interact") > 0) //user interaction
-        //if (Input.GetAxisRaw("Back") > 0) //abort! abort! you know, dialogue
+        if (Input.GetAxisRaw("Interact") > 0) //user interaction
+        {
+            GameObject objectHit = InteractCheck(directionV);
+            if (objectHit != null)
+            {
+                if (objectHit.GetComponent<NPC_Dialogue>())
+                {
+                    NPC_Dialogue NPC = objectHit.GetComponent<NPC_Dialogue>();
+                    NPC.Interact(); 
+                }
+            }
+        }
+        if (Input.GetAxisRaw("Back") > 0) //abort! abort! you know, dialogue
+        {
+
+        }
+    }
+
+    bool CollisionCheck(Vector2 direction)
+    {
+        if (Physics2D.Raycast(transform.position, direction, 2.0f, layerMaskCollisions))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    GameObject InteractCheck(Vector2 direction)
+    {
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, direction, 2.0f, layerMaskInteracts);
+        if(raycastHit2D)
+        {
+            return raycastHit2D.collider.gameObject;
+        }
+        return null;
     }
 }

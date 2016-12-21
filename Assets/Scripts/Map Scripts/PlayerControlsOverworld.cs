83aec933535793794f9e5b930ffe2147253c274c
Omer Ahmed <omer1@ualberta.ca>
2016-12-21 04:34:24 -0700

    More Than Almost Pause
    -Pause menu appears and disappears when you press the pause key (Escape
    or 'P')
    -The 'Resume' button on the pause menu unpauses the game
    -The player stops moving and cannot move further while the game is
    paused
    -Gave Kira some riveting dialogue, although she's still Chip in disguise
    right now.

--- Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
+++ Assets/Scripts/Map Scripts/PlayerControlsOverworld.cs
@@ -6,12 +6,18 @@ public class PlayerControlsOverworld : MonoBehaviour {
 
     Vector3 pos;                    // For movement
     public float speed = 4.0f;      // Speed of movement
+	public bool paused = false;
     public bool canMove = true;
     public Vector2 directionV;
     public int layerMaskCollisions = 1 << 8;
     public int layerMaskInteracts = 1 << 9;
     GameObject canvas;
-    GameObject UI_DialogueSystem;
+	GameObject UI_DialogueSystem;
+	GameObject UI_PauseMenu;
+	GameObject UI_PauseMenuRoot;
+
+	public bool pauseButtonDown = false;
+	public bool pauseButtonPrevious = false;
 
     void Start()
     {
@@ -19,77 +25,64 @@ public class PlayerControlsOverworld : MonoBehaviour {
 		directionV = Vector2.down;
 		canvas = GameObject.Find("Canvas");   //finding the Canvas gameObject
 		UI_DialogueSystem = canvas.transform.Find("UI_DialogueSystem").gameObject;
+		UI_PauseMenu = canvas.transform.Find("UI_PauseMenu").gameObject;
+		UI_PauseMenuRoot = UI_PauseMenu.transform.Find("UI_PauseMenuRoot").gameObject;
     }
 
     void Update()
     {
-        if (canMove)
-        {
-            if (Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos)   // Left
-            {
-                directionV = Vector2.left;
-                if (CollisionCheck(directionV))
-                {
-                    GameObject objectHit = InteractCheck(directionV);
-                    pushing(objectHit);
-                    //Walk into wall sound effect
-                }
-                else
-                {
-                    pos += 2 * Vector3.left;
-                }
-            }
-            if (Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos)   // Right
-            {
-                directionV = Vector2.right;
-                if (CollisionCheck(directionV))
-                {
-                    {
-                        GameObject objectHit = InteractCheck(directionV);
-                        pushing(objectHit);
-                        //Walk into wall sound effect
-                    }
-                }
-                else
-                {
-                    pos += 2 * Vector3.right;
-                }
-            }
-            if (Input.GetAxisRaw("Vertical") > 0 && transform.position == pos)   // Up
-            {
-                directionV = Vector2.up;
-                if (CollisionCheck(directionV))
-                {
-                    {
-                        GameObject objectHit = InteractCheck(directionV);
-                        pushing(objectHit);
-                        //Walk into wall sound effect
-                    }
-                }
-                else
-                {
-                    pos += 2 * Vector3.up;
-                }
-            }
-            if (Input.GetAxisRaw("Vertical") < 0 && transform.position == pos)   // Down
-            {
-                directionV = Vector2.down;
-                if (CollisionCheck(directionV))
-                {
-                    {
-                        GameObject objectHit = InteractCheck(directionV);
-                        pushing(objectHit);
-                        //Walk into wall sound effect
-                    }
-                }
-                else
-                {
-                    pos += 2 * Vector3.down;
-                }
-            }
-        }
+		if (!paused) {
+			if (canMove) {
+				if (Input.GetAxisRaw ("Horizontal") < 0 && transform.position == pos) {   // Left
+					directionV = Vector2.left;
+					if (CollisionCheck (directionV)) {
+						GameObject objectHit = InteractCheck (directionV);
+						pushing (objectHit);
+						//Walk into wall sound effect
+					} else {
+						pos += 2 * Vector3.left;
+					}
+				}
+				if (Input.GetAxisRaw ("Horizontal") > 0 && transform.position == pos) {   // Right
+					directionV = Vector2.right;
+					if (CollisionCheck (directionV)) {
+						{
+							GameObject objectHit = InteractCheck (directionV);
+							pushing (objectHit);
+							//Walk into wall sound effect
+						}
+					} else {
+						pos += 2 * Vector3.right;
+					}
+				}
+				if (Input.GetAxisRaw ("Vertical") > 0 && transform.position == pos) {   // Up
+					directionV = Vector2.up;
+					if (CollisionCheck (directionV)) {
+						{
+							GameObject objectHit = InteractCheck (directionV);
+							pushing (objectHit);
+							//Walk into wall sound effect
+						}
+					} else {
+						pos += 2 * Vector3.up;
+					}
+				}
+				if (Input.GetAxisRaw ("Vertical") < 0 && transform.position == pos) {   // Down
+					directionV = Vector2.down;
+					if (CollisionCheck (directionV)) {
+						{
+							GameObject objectHit = InteractCheck (directionV);
+							pushing (objectHit);
+							//Walk into wall sound effect
+						}
+					} else {
+						pos += 2 * Vector3.down;
+					}
+				}
+			}
 
-        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Moving to new pos
+	        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Moving to new pos
+		}
 
         if (Input.GetAxisRaw("Interact") > 0) //user interaction
         {
@@ -103,11 +96,38 @@ public class PlayerControlsOverworld : MonoBehaviour {
                     NPC.Interact(); 
                 }
             }
-        }
+		}
+		if (Input.GetAxisRaw ("Pause") > 0) { //pause, pause, pause everything please! bring up the menu make everything stop
+			pauseButtonDown = true;
+		} else {
+			pauseButtonDown = false;
+		}
+		if(pauseButtonDown == true && pauseButtonPrevious == false){
+			if (paused) { //Unpausing
+				UI_PauseMenuRoot.SetActive (false);
+				UI_PauseMenu.SetActive (false);
+				paused = false;
+			} else {
+				if (!UI_DialogueSystem.activeSelf) { //If the dialogue box is not open/active
+					UI_PauseMenu.SetActive (true);
+					UI_PauseMenuRoot.SetActive (true);
+					paused = true;
+				}
+			}
+		}
         if (Input.GetAxisRaw("Back") > 0) //abort! abort! you know, dialogue
         {
-			UI_DialogueSystem.SetActive(false);
+			if (paused) { //Unpausing
+				UI_PauseMenuRoot.SetActive (false);
+				UI_PauseMenu.SetActive (false);
+				paused = false;
+			}
+			if (UI_DialogueSystem.activeSelf) {
+				UI_DialogueSystem.SetActive (false);
+			}
         }
+
+		pauseButtonPrevious = pauseButtonDown;
     }
 
     bool CollisionCheck(Vector2 direction)
@@ -128,6 +148,35 @@ public class PlayerControlsOverworld : MonoBehaviour {
         }
         return null;
     }
+
+	public void PauseMenuButtons(string pressedButton = "resume")
+	{
+		switch (pressedButton) {
+			case "resume":
+				UI_PauseMenuRoot.SetActive (false);
+				UI_PauseMenu.SetActive (false);
+				paused = false;
+				break;
+			case "party":
+				//UI_PauseMenuRoot.SetActive (false);
+				//UI_PauseMenu.SetActive (false);
+				//paused = false;
+				break;
+			case "missions":
+				//UI_PauseMenuRoot.SetActive (false);
+				//UI_PauseMenu.SetActive (false);
+				//paused = false;
+				break;
+			case "journal":
+				//UI_PauseMenuRoot.SetActive (false);
+				//UI_PauseMenu.SetActive (false);
+				//paused = false;
+				break;
+			case "quit":
+				Application.Quit ();
+				break;
+		}
+	}
 
     void pushing(GameObject objectHit)
     {

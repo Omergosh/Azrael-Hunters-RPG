8fec2b9ce1399e0ed6001d4280771af0dea8e20c
Struckdown <bfleysher2@gmail.com>
2017-01-14 22:13:34 -0700

    Title Screen and First Pitch for Battle Screen added
    -Title screen updated and now has a "Press Any Key to Continue" feature
    -Battle Scene updated for concept pitch

---
+++ Assets/Scripts/Battle Scripts/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    float barDisplay = 0;
+    public Vector2 pos;
+    public Vector2 size = new Vector2(60, 20);
+    Texture2D progressBarEmpty;
+    Texture2D progressBarFull;
+
+    void Start()
+    {
+        //pos = transform.position;
+    }
+
+    void OnGUI()
+    {
+        //pos = transform.position;
+        // draw the background:
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), progressBarEmpty);
+
+        // draw the filled-in part:
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), progressBarFull);
+        GUI.EndGroup();
+
+        GUI.EndGroup();
+
+    }
+
+    void Update()
+    {
+        // for this test, the bar display is linked to the current time
+        // However, we'll need to set it to the player's health later
+        barDisplay = Time.time * 0.05f;
+    }
+}
---
+++ Assets/Scripts/Title Screen/TitleScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+	
+    public string mainMenu;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(mainMenu);
+        }
+	}
+}

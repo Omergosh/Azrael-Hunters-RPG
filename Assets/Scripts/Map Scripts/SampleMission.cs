080b685118193a47b9d1e29889fb796ec3acf5bf
Omer Ahmed <omer1@ualberta.ca>
2017-01-16 13:34:36 -0700

    Pushy
    - Aligned stuff on Battle Screen
    - Not much else

---
+++ Assets/Scripts/Map Scripts/SampleMission.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleMission : MonoBehaviour {
+
+	//GameObjects & Displays
+	public Button buttonComponent;
+	public Image questGiverPortrait;
+	public Text missionSummary;
+	public Text missionLocation;
+	public Text missionReward;
+
+	//Actual mission data/information
+	public int expReward = 10;
+	//public List<Items>
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

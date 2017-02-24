ca42c7eec8a4873a57f3312c55c8a19840baee42
Struckdown <bfleysher@gmail.com>
2017-02-23 20:01:30 -0700

    Small update to BattleManager
    -Colliders added
    -BattleManager now allows for potential transitions
    -Player characters can be selected through Raycasts

--- Assets/Scripts/Battle Scripts/BaseEnemy.cs
+++ Assets/Scripts/Battle Scripts/BaseEnemy.cs
@@ -3,36 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BasePlayer: MonoBehaviour {
+public class BaseEnemy : MonoBehaviour
+{
 
-    //public GameObject player;
+    //public GameObject enemy;
     public int currentHealth = 728;
     public int maxHealth = 1525;
 
-    public bool isPlayerCharacter = true;
+    public bool isPlayerCharacter = false;
     public Image healthBar;
     public Text healthText;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         //healthBar = transform.FindChild("HealthBar").gameObject.GetComponent<Image>();
         //healthText = transform.FindChild("HealthText").gameObject.GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //temporary health bar tester
-        if(currentHealth > 0)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // temporary health bar tester
+        if (currentHealth > 0)
         {
             currentHealth -= 1;
         }
         //updateHealthBar();
-	}
+    }
 
     void updateHealthBar()
     {
         healthText.text = currentHealth.ToString() + " HP";
-        healthBar.transform.localScale = new Vector3(Mathf.Clamp(((float)currentHealth/ maxHealth), 0, 1), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp(((float)currentHealth / maxHealth), 0, 1), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
     }
 }
--- Assets/Scripts/Battle Scripts/BattleManager.cs
+++ Assets/Scripts/Battle Scripts/BattleManager.cs
@@ -6,13 +6,15 @@ public class BattleManager : MonoBehaviour {
 
     //This class manages turn order, actions and calls on other classes to appropriately apply methods
 
+    public List<Transform> playerCharacterList;
+    public List<Transform> enemyList;
 
-    public List<Transform> combatantsList; 
+    public GameObject selectedCharacter;
 
     public enum phaseState {
-        SELECTING,     // player is selecting an action
-        STARTBATTLE,    // initializing things for battle like models and stuff
-        NEWROUND,   // new turn order at the end of each round
+        SELECTING,     // player is selecting a character
+        PROCESSING, // something is happening!
+        NEWROUND,   // new turn order at the end of each round, plus checking Buffs/Debuffs
         VICTORY,    // battle won
         DEFEAT     // battle lost
     }
@@ -20,22 +22,61 @@ public class BattleManager : MonoBehaviour {
     public phaseState currentState;
 
 	// Use this for initialization
-	void Start () {
-        combatantsList = new List<Transform>();
+	void Start () {// initializing things for battle such as models and stuff
 
+        foreach(Transform child in transform)   // adding each combatant to one of two lists: player or enemy
+        {
+            if(child.GetComponent<BasePlayer>() != null)
+            {
+                playerCharacterList.Add(child);
+            }
+
+            else
+            {
+                enemyList.Add(child);
+            }
+        }
+       
+
+        // intimidation phase here
+
+        // intimidation ends
+
+        currentState = phaseState.SELECTING;   // initialize to player select turn
     }
 	
 	// Update is called once per frame
 	void Update () {
         switch (currentState)
         {
-            case (phaseState.SELECTING):
-                //do stuff here
+            case (phaseState.SELECTING):    // selecting a character to use
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //Converting Mouse Pos to 2D (vector2) World Pos
+                    RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+                    if (hit.collider.GetComponent<BasePlayer>() != null)
+                    {
+                        Debug.Log("You clicked a Friendly Chip!");
+                        Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                        selectedCharacter = hit.transform.gameObject;
+                        currentState = phaseState.PROCESSING;
+                    }
+
+                }
+
+                if (enemyList.Count == 0)    // checking if enemies have been defeated
+                {
+                    currentState = phaseState.VICTORY;    // disabled until enemies added
+                }
+
                 break;
 
-            case (phaseState.STARTBATTLE):
+            case (phaseState.PROCESSING):
 
                 break;
+
             case (phaseState.NEWROUND):
 
                 break;

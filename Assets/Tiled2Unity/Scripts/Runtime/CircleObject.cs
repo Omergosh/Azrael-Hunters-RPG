f3d5f04f1d28aa85ddb22354484c24fb25c4de82
Struckdown <bfleysher@gmail.com>
2018-04-25 15:04:23 -0600

    Build update
    Updated my Visual Studios and Unity versions = Fixed some script errors
    Game now builds again

--- Assets/Tiled2Unity/Scripts/Runtime/CircleObject.cs
+++ Assets/Tiled2Unity/Scripts/Runtime/CircleObject.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using UnityEditor;
-
-namespace Tiled2Unity
+﻿namespace Tiled2Unity
 {
     public class CircleObject : TmxObject
     {
